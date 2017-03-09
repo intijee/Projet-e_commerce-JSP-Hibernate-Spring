45640fcec63436c7a5572a771960bf8df17d7e1b
inti0297 <inti0297@192.168.1.78>
2017-03-09 09:03:57 +0100

    toucher au test

--- TP_Spring_Chevet_Le_Floch/src/test/java/fr/adaming/serviceTest/TestService.java
+++ TP_Spring_Chevet_Le_Floch/src/test/java/fr/adaming/serviceTest/TestService.java
@@ -1,5 +1,68 @@
 package fr.adaming.serviceTest;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//choisir le runner de spring
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
 public class TestService {
+	@Autowired
+	IService service;
+	
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void ajouterProduitTest(){
+		
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void modifierProduitTest(){
+		
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void supprimerProduitTest(){
+		
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void afficherProduitTest(){
+		
+	}
+	
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void ajouterCatTest(){
+		
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void modifierCatTest(){
+		
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void supprimerCatTest(){
+		
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void afficherCatTest(){
+		
+	}
 
 }

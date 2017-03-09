8cfe52a5bb0a67c1fea4237494d201c3bb2caa9f
inti0297 <inti0297@DESKTOP-BLNLL7N.home>
2017-03-09 10:48:37 +0100

    niksamere

--- TP_Spring_Chevet_Le_Floch/src/test/java/fr/adaming/serviceTest/TestService.java
+++ TP_Spring_Chevet_Le_Floch/src/test/java/fr/adaming/serviceTest/TestService.java
@@ -1,5 +1,6 @@
 package fr.adaming.serviceTest;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,61 +9,63 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-//choisir le runner de spring
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
+import fr.adaming.service.IAdminService;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+////choisir le runner de spring
+//@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
 public class TestService {
-	@Autowired
-	IService service;
-	
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void ajouterProduitTest(){
-		
-	}
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void modifierProduitTest(){
-		
-	}
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void supprimerProduitTest(){
-		
-	}
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void afficherProduitTest(){
-		
-	}
-	
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void ajouterCatTest(){
-		
-	}
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void modifierCatTest(){
-		
-	}
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void supprimerCatTest(){
-		
-	}
-	@Test
-	@Rollback(true)
-	@Transactional
-	public void afficherCatTest(){
-		
-	}
+//	@Autowired
+//	IAdminService adminService;
+////	
+//	@Ignore
+//	@Rollback(true)
+//	@Transactional
+//	public void ajouterProduitTest(){
+//		
+////	}
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void modifierProduitTest(){
+//		
+//	}
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void supprimerProduitTest(){
+//		
+//	}
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void afficherProduitTest(){
+//		
+//	}
+//	
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void ajouterCatTest(){
+//		
+//	}
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void modifierCatTest(){
+//		
+//	}
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void supprimerCatTest(){
+//		
+//	}
+//	@Test
+//	@Rollback(true)
+//	@Transactional
+//	public void afficherCatTest(){
+//		
+//	}
 
 }
